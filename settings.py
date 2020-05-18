30d055b4fca968b28eed944b3c9cab852d895818
akhile-a <65538561+akhile-a@users.noreply.github.com>
2020-05-18 15:57:32 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*5sokb^l(%-anv!8539%(i^^wq5#&_3+@h41ws(-8jj@7zt2=a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['firsttail.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['firsttail.herokuapp.com' ,'127.0.0.1']
 
 
 # Application definition
